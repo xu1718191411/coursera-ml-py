c05de110bfb4eecef5fa9a1855658adfca6c00ef
xu1718191411 <xu1718191411@gmail.com>
2020-04-04 07:00:53 +0900

    Add 1. stop using reshape into specific shape

--- machine-learning-ex2/ex2/costFunction.py
+++ machine-learning-ex2/ex2/costFunction.py
@@ -11,16 +11,15 @@ def cost_function(theta, X, y):
 
     h = sigmoid(np.dot(X,theta))
 
-    cost = (1/m) * np.sum(-y * np.log(h) - (1 - y) * np.log(-h + 1),0)
+    mt = 1e-7
+    cost = (1 / m) * np.sum(-y * np.log(h + mt) - (1 - y) * np.log(-h + 1 + mt), 0)
 
-    grad = (1/m) * np.reshape((h - y),[1,100]).dot(X)
+    grad = (1 / m) * np.dot(X.T, (h - y))
 
     # ===================== Your Code Here =====================
     # Instructions : Compute the cost of a particular choice of theta
     #                You should set cost and grad correctly.
     #
-
-
 
     # ===========================================================
 
